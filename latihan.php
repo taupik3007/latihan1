0581bddef6b1db7ca7de2e800042dbc0e5488629
taupik3007 <taupikpathurrohman@gmail.com>
2022-11-15 22:31:56 +0700

    first commit

---
+++ latihan.php
@@ -0,0 +1,24 @@
+<?php
+
+// $awikwok = true;
+
+// echo 10;
+
+$gararetek = [["wikwok","gararetek"], ["wikwok","gararetek"] , ["wikwok","gararetek"]];
+// var_dump($gararetek);
+// echo $gararetek;
+
+foreach($gararetek as $udin){
+    foreach($udin as $jj){
+        echo $jj;
+    }
+}
+
+
+// for($i=0;$i<10;$i++){
+//     echo $udin;
+// }
+
+
+
+?>
